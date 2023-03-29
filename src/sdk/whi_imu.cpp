2905f4fc4567cb07a1632b425a5ec6f00d599839
Xinjue Zou <hit_zou@126.com>
2023-03-29 13:34:58 +0800

    revise namespace to relative for multiple robots

--- src/sdk/whi_imu.cpp
+++ src/sdk/whi_imu.cpp
@@ -39,10 +39,10 @@ namespace whi_motion_interface
         std::string dataTopic;
         std::string magTopic;
         std::string tempTopic;
-        node_handle_->param("/whi_imu/frame_id", frameId, std::string("imu_link"));
-        node_handle_->param("/whi_imu/data_topic", dataTopic, std::string("imu_data"));
-        node_handle_->param("/whi_imu/mag_topic", magTopic, std::string("mag_data"));
-        node_handle_->param("/whi_imu/temp_topic", tempTopic, std::string("temp_data"));
+        node_handle_->param("whi_imu/frame_id", frameId, std::string("imu_link"));
+        node_handle_->param("whi_imu/data_topic", dataTopic, std::string("imu_data"));
+        node_handle_->param("whi_imu/mag_topic", magTopic, std::string("mag_data"));
+        node_handle_->param("whi_imu/temp_topic", tempTopic, std::string("temp_data"));
         std::string module;
         std::string port;
         int baudrate = 0;
@@ -52,15 +52,15 @@ namespace whi_motion_interface
         int instructionMinSpan = 5;
         bool withMag = false;
         bool withTemp = false;
-        node_handle_->param("/whi_imu/hardware_interface/module", module, std::string(type_str[WIT_JY61P]));
-        node_handle_->param("/whi_imu/hardware_interface/port", port, std::string("/dev/ttyUSB0"));
-        node_handle_->param("/whi_imu/hardware_interface/baudrate", baudrate, 9600);
-        node_handle_->param("/whi_imu/hardware_interface/pack_length", packLength, 11);
-        node_handle_->getParam("/whi_imu/hardware_interface/reset_yaw", *resetList);
-        node_handle_->getParam("/whi_imu/hardware_interface/unlock", *unlockList);
-        node_handle_->param("/whi_imu/hardware_interface/instruction_min_span", instructionMinSpan, 5);
-        node_handle_->param("/whi_imu/hardware_interface/with_magnetic", withMag, true);
-        node_handle_->param("/whi_imu/hardware_interface/with_temperature", withTemp, false);
+        node_handle_->param("whi_imu/hardware_interface/module", module, std::string(type_str[WIT_JY61P]));
+        node_handle_->param("whi_imu/hardware_interface/port", port, std::string("/dev/ttyUSB0"));
+        node_handle_->param("whi_imu/hardware_interface/baudrate", baudrate, 9600);
+        node_handle_->param("whi_imu/hardware_interface/pack_length", packLength, 11);
+        node_handle_->getParam("whi_imu/hardware_interface/reset_yaw", *resetList);
+        node_handle_->getParam("whi_imu/hardware_interface/unlock", *unlockList);
+        node_handle_->param("whi_imu/hardware_interface/instruction_min_span", instructionMinSpan, 5);
+        node_handle_->param("whi_imu/hardware_interface/with_magnetic", withMag, true);
+        node_handle_->param("whi_imu/hardware_interface/with_temperature", withTemp, false);
         transform(module.begin(), module.end(), module.begin(), ::tolower);
         if (module == type_str[WIT_JY61P] || module == type_str[WIT_JY901] )
         {
@@ -76,7 +76,7 @@ namespace whi_motion_interface
         srv_reset_ = std::make_unique<ros::ServiceServer>(node_handle_->advertiseService("imu_reset", &Imu::onServiceReset, this));
 
         // spinner
-        node_handle_->param("/whi_imu/loop_hz", loop_hz_, 10.0);
+        node_handle_->param("whi_imu/loop_hz", loop_hz_, 10.0);
         ros::Duration updateFreq = ros::Duration(1.0 / loop_hz_);
         non_realtime_loop_ = std::make_unique<ros::Timer>(node_handle_->createTimer(updateFreq, std::bind(&Imu::update, this, std::placeholders::_1)));
     }
